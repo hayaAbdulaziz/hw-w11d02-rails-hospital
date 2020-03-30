466719ba287316eae660e094822e4a4501e1316a
hayaAbdulaziz <hayal5066@gmail.com>
2020-03-30 20:08:08 +0300

    creat view a single patient and view all patients

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'patients/:id' , to: 'patients#show' , as:'patient'
+  get 'patients', to: 'patients#index'
+  get 'todos/new', to: 'todos#new'
+  # post 'patients', to: 'patients#create'
+  get 'pages/home'
+  root 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
